2f6c893d332a794bafd4db488a07618923ed276a
ardigermenji <ardi.germenji@gmail.com>
2021-03-24 15:08:07 +0100

    adds SingleBook components

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
 
-export default function SingleBook({ book }) {
-  return <div></div>;
+export default function SingleBook({ img, title }) {
+  return (
+    <Card>
+      <Card.Img variant='top' src={img} alt='book cover' />
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        {/* <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text> */}
+      </Card.Body>
+    </Card>
+  );
 }
