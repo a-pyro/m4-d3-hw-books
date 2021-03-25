3b9f45ab16f6a551362c12a33087e756ca825f4a
ardigermenji <ardi.germenji@gmail.com>
2021-03-25 14:27:11 +0100

    sd

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,6 @@
 import { Row, Col } from 'react-bootstrap';
 
 export default function Footer(props) {
-  // const { className } = props;
   return (
     <Row
       className='bg-dark text-white justify-content-center mx-0'
