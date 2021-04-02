327435a9d16b0c954888ef3fdab4be71f2b7103c
ardigermenji <ardi.germenji@gmail.com>
2021-04-02 16:53:34 +0200

    adds select to load differennt book categories

--- src/App.js
+++ src/App.js
@@ -1,22 +1,74 @@
+import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 // import WarningSing from './components/WarningSing';
 // import MyBadge from './components/MyBadge';
 import BookList from './components/BookList';
 import fantasy from './data/fantasy';
+import history from './data/history.json';
+import horror from './data/horror.json';
+import romance from './data/romance.json';
+import scifi from './data/scifi.json';
 import MyNav from './components/MyNav';
 import Wellcome from './components/Wellcome';
 import Footer from './components/Footer';
 
-function App() {
-  return (
-    <>
-      <MyNav title='BookStore' />
-      <Wellcome />
-      <BookList listOfBooks={fantasy} />
-      <Footer />
-    </>
-  );
+class App extends Component {
+  state = {
+    listOfBooks: [],
+    category: '',
+  };
+
+  componentDidMount = () => {
+    this.setState({ listOfBooks: fantasy });
+  };
+
+  handleCategoryChange = (cat) => {
+    this.setState({ category: cat });
+  };
+
+  componentDidUpdate = (prevProps, prevState) => {
+    console.log('App did update');
+    if (this.state.category !== prevState.category) {
+      let newCatToLoad;
+      switch (this.state.category) {
+        case 'Fantasy':
+          newCatToLoad = fantasy;
+          break;
+        case 'History':
+          newCatToLoad = history;
+
+          break;
+
+        case 'Horror':
+          newCatToLoad = horror;
+
+          break;
+        case 'Romance':
+          newCatToLoad = romance;
+
+          break;
+        case 'Scifi':
+          newCatToLoad = scifi;
+
+          break;
+
+        default:
+          break;
+      }
+    }
+  };
+
+  render() {
+    return (
+      <>
+        <MyNav title='BookStore' />
+        <Wellcome handleCategoryChange={this.handleCategoryChange} />
+        <BookList listOfBooks={this.state.listOfBooks} />
+        <Footer />
+      </>
+    );
+  }
 }
 
 export default App;
--- src/components/Wellcome.jsx
+++ src/components/Wellcome.jsx
@@ -1,15 +1,34 @@
-import React from 'react';
-import { Jumbotron, Container } from 'react-bootstrap';
+import React, { Component } from 'react';
 
-export default function Wellcome() {
-  return (
-    <Jumbotron fluid className='py-2'>
-      <Container>
-        <div className='d-flex flex-column align-items-center'>
-          <h1>Wellcome to the Bookstore</h1>
-          <p>This is a little introduction I had to write</p>
-        </div>
-      </Container>
-    </Jumbotron>
-  );
+import { Jumbotron, Container, Form } from 'react-bootstrap';
+
+export default class Wellcome extends Component {
+  handleChange = (e) => {
+    const category = e.target.value.toLowerCase();
+    console.log(category);
+    this.props.handleCategoryChange(category);
+  };
+  render() {
+    return (
+      <Jumbotron fluid className='py-2'>
+        <Container>
+          <div className='d-flex flex-column align-items-center'>
+            <h1>Wellcome to the Bookstore</h1>
+            <p>Select a book category</p>
+            <Form>
+              <Form.Group>
+                <Form.Control onChange={this.handleChange} as='select' custom>
+                  <option>Fantasy</option>
+                  <option>History</option>
+                  <option>Horror</option>
+                  <option>Romance</option>
+                  <option>Scifi</option>
+                </Form.Control>
+              </Form.Group>
+            </Form>
+          </div>
+        </Container>
+      </Jumbotron>
+    );
+  }
 }
